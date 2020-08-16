e2071d2b3b905d677a29e8cbbb5c698b8fc6116c
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-08-16 09:26:01 +0700

    add history route

--- apps/router_navigation.js
+++ apps/router_navigation.js
@@ -3,9 +3,11 @@ const route = require('express').Router()
 // import routes
 const product = require('./routes/r_product')
 const category = require('./routes/r_category')
+const history = require('./routes/r_history')
 
 // buat middle 
 route.use('/product', product)
 route.use('/category', category)
+route.use('/history', history)
 
 module.exports = route
