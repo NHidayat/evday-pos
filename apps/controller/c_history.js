449bb05b53037fce4ccf70f7fe6914db2c241300
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-08-17 13:05:21 +0700

    change model/control method

--- apps/controller/c_history.js
+++ apps/controller/c_history.js
@@ -1,4 +1,5 @@
-const { getAllHistory, getHistoryById, getItemByHistory, getHistoryCount } = require('../model/m_history')
+const { getAllHistory, getHistoryById, getItemByHistory, postHistory, postOrder } = require('../model/m_history')
+const { postOrderItem } = require('../model/m_order')
 const helper = require('../helper/my_helper')
 
 module.exports = {
@@ -17,12 +18,32 @@ module.exports = {
             const result = await getHistoryById(id, cartResult)
 
             if (result.length > 0) {
-                return helper.response(response, 200, "Success Get History", result)
+                return helper.response(response, 200, `Success Get History by ID ${id}`, result)
             } else {
                 return helper.response(response, 404, `History by ID ${id} not found`, result)
             }
         } catch(error) {
             return helper.response(response, 400, "Bad Request", error)
         }
-    }
+    },
+    postHistory: async (request, response) => {
+        try {
+            const itemsData = request.body.items
+            const history_invoice = new Date().getTime()
+            const itemsTotal = itemsData.map(item => item.subtotal).reduce((a, b) => a + b)
+            const history_ppn = (0.1 * itemsTotal)
+            const history_total = itemsTotal + history_ppn
+            const invoiceData = {
+                history_invoice,
+                history_ppn,
+                history_total,
+                history_created_at: new Date()
+            }
+            const insertHistory = await postHistory(invoiceData, itemsData)
+            const result = await postOrderItem(insertHistory,itemsData)
+            return helper.response(response, 200, "Success Post History", result)
+        } catch(error) {
+         return helper.response(response, 400, "Bad Request", error)
+     }
+ }
 }
--- apps/model/m_history.js
+++ apps/model/m_history.js
@@ -4,21 +4,20 @@ module.exports = {
 	getItemByHistory: (id) => {
 		return new Promise((resolve, reject) => {
 			connection.query('SELECT * FROM order_item WHERE history_id = ?', id, (error, result) => {
-				!error ? resolve(result) : reject(new Eror(error))
+				!error ? resolve(result) : reject(new Error(error))
 			})
 		})
 	},
 	getAllHistory: () => {
 		return new Promise((resolve,reject) => {
 			connection.query('SELECT * FROM history', (error, result) => {
-				!error ? resolve(result) : reject(new Eror(error))
+				!error ? resolve(result) : reject(new Error(error))
 			})
 		})
 	},
 	getHistoryById: (id, cartItem) => {
 		return new Promise((resolve,reject) => {
 			connection.query('SELECT * FROM history WHERE history_id = ?', id, (error, result) => {
-			// connection.query('SELECT * FROM history h JOIN order_item o ON (h.history_id = o.history_id) WHERE h.history_id = ?', id, (error, result) => {
 				if (!error) {
 					if (result.length > 0) {
 						result[0].items = cartItem
@@ -32,11 +31,45 @@ module.exports = {
 			})
 		})
 	},
-	getHistoryCount: () => {
+	postHistory: (setData, itemsData) => {
 		return new Promise((resolve, reject) => {
-			connection.query('SELECT COUNT(*) AS total FROM history', (error, result) => {
-				!error ? resolve(result[0].total) : reject(new Eror(error))
+			connection.query('INSERT INTO history SET ?', setData, (error, result) => {
+				if (!error) {
+					const insertId = result.insertId
+					const { history_invoice, history_created_at, history_ppn, history_total } = setData
+					const newResult = {
+						history_id: result.insertId,
+						...setData
+					}
+					resolve(newResult)
+				} else {
+					reject(new Error(error))
+				}
 			})
 		})
 	},
+	postOrder: (setData) => {
+		return new Promise((resolve, reject) => {
+			connection.query('INSERT INTO order_item SET ?', setData, (error, result) => {
+				!error ? resolve(result) : reject(new Error(error))
+			})
+		})
+	}
+
+}
+
+const postOrderItem = (setData) => {
+	return new Promise((resolve, reject) => {
+		connection.query('INSERT INTO order_item SET ?', setData, (error, result) => {
+			if (!error) {
+				const newResult = {
+					history_id: result.insertId,
+					...setData
+				}
+				resolve(newResult)
+			} else {
+				reject(new Error(error))
+			}
+		})
+	})
 }
