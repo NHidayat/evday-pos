73cb4186ff258c0ad3355aa21d6a78e6eba29365
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-08-17 12:58:26 +0700

    add order model

---
+++ apps/model/m_order.js
@@ -0,0 +1,36 @@
+const connection = require('../config/mysql')
+
+module.exports = {
+	postOrderItem : (historyData, itemsData) => {
+		return new Promise((resolve, reject) => {
+
+			const setData = itemsData.map(v => {
+				const setItemData = {
+					history_id: historyData.history_id,
+					product_id: v.product_id,
+					product_name: v.product_name,
+					qty: v.qty,
+					subtotal: v.subtotal,
+				}
+				connection.query('INSERT INTO order_item SET ?', setItemData, (error, result) => {
+					if (!error) {
+						const { history_id, history_invoice, history_created_at, history_ppn, history_total } = historyData
+						const newResult = {
+							history_id,
+							history_invoice,
+							history_created_at,
+							items: itemsData,
+							history_ppn,
+							history_total
+						}
+						resolve(newResult)
+
+					} else {
+						reject(new Error(error))
+					}
+				})
+				
+			})
+		})
+	}
+}
