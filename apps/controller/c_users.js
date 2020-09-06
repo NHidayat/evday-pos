f68ff03a3e6477b675f3b7c7106fbc2d69367adc
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-09-06 20:58:52 +0800

    add register validation

--- apps/controller/c_users.js
+++ apps/controller/c_users.js
@@ -19,36 +19,46 @@ module.exports = {
         }
 
         try {
-            const result = await postUser(setData)
-            return helper.response(response, 200, 'Success Register User', result)
+            const checkUserData = await cekUser(user_email)
+            if (checkUserData.length > 0) {
+                return helper.response(response, 403, 'This email already rigistered ')
+            } else {
+                const result = await postUser(setData)
+                return helper.response(response, 200, 'Success Register User', result)
+            }
         } catch (e) {
-            return helper.response(response, 400, 'Bad Request')
+            return helper.response(response, 400, 'Bad Request', e)
         }
     },
     loginUser: async (request, response) => {
         try {
             const { user_email, user_password } = request.body
-            const checkkUserData = await cekUser(user_email)
+            const checkUserData = await cekUser(user_email)
 
-            if (checkkUserData.length >= 1) {
+            if (checkUserData.length >= 1) {
                 console.log(true)
-                const checkPassword = bcrypt.compareSync(user_password, checkkUserData[0].user_password)
-
-                if (checkPassword) {
-                    const { user_id, user_email, user_password, user_name, user_role, user_status } = checkkUserData[0]
-
-                    let payload = {
-                        user_id,
-                        user_email,
-                        user_role,
-                        user_status
-                    }
-                    const token = jwt.sign(payload, 'RAHASIA', { expiresIn: 3600 * 24 })
-                    payload = { ...payload, token }
-                    return helper.response(response, 200, 'Success Login', payload)
+                if (checkUserData[0].user_status !== 1) {
+                    return helper.response(response, 403, "Your account is not active, please contact the admin")
                 } else {
-                    return helper.response(response, 400, 'Wrong Password')
+                    const checkPassword = bcrypt.compareSync(user_password, checkUserData[0].user_password)
+
+                    if (checkPassword) {
+                        const { user_id, user_email, user_password, user_name, user_role, user_status } = checkUserData[0]
+
+                        let payload = {
+                            user_id,
+                            user_email,
+                            user_role,
+                            user_status
+                        }
+                        const token = jwt.sign(payload, 'RAHASIA', { expiresIn: 3600 * 24 })
+                        payload = { ...payload, token }
+                        return helper.response(response, 200, 'Success Login', payload)
+                    } else {
+                        return helper.response(response, 400, 'Wrong Password')
+                    }
                 }
+
             } else {
                 return helper.response(response, 400, 'Email not Registed')
             }
